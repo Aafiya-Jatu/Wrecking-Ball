4dfeb1e739f20f83ba056542757d9f60208ea8b8
Aafiya-Jatu <72740805+Aafiya-Jatu@users.noreply.github.com>
2020-12-24 16:57:48 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,19 @@
+class Ball{
+    constructor(x,y,r){
+    var options = {
+    density: 1,
+    frictionAir: 0.05,
+    }
+    this.body = Bodies.circle(x,y,r,options);
+    this.r = r;
+    World.add(world,this.body);
+    }
+display(){
+    var pos = this.body.position;
+    push();
+    translate(pos.x,pos.y);
+    rotate(this.body.angle);
+    ellipse(0,0,this.r,this.r);
+    pop();
+}
+}
